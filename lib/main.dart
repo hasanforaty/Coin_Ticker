f7e7ad2cdc1c0ee052cb805dfe7d735bd55e8a65
hasan foraty <hasanforaty@gmail.com>
2022-12-31 22:32:42 +0330

    create main body and app bar

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,41 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.light(),
-      home:,
+      home: const MainBody(),
+    );
+  }
+}
+
+class MainBody extends StatelessWidget {
+  const MainBody({Key? key}) : super(key: key);
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0.0,
+      flexibleSpace: Container(
+        decoration: const BoxDecoration(
+            gradient:
+                LinearGradient(colors: [Color(0xffc9ffbf), Color(0xffffafbd)])),
+      ),
+      title: const Text("Convert "),
+      leading: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        child: Image.asset(
+          "images/bitcoin.png",
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(18),
+        ),
+      ),
     );
   }
 }
